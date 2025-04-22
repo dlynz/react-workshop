781ebf8a333f3a484eee70b6e012940bdffc85ec
louis <louislim2005@gmail.com>
2025-04-22 19:32:28 +1000

    Added a button that changes colour on hover

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className=''>
+      <div className='flex flex-col items-center justify-center'>
         {/* START HERE */}
         <p className='font-bold text-2xl'>
           Hellooooo!!
         </p>
+        <button className='bg-blue-500 hover:bg-red-600 transition duration-300 text-white font-bold py-2 px-4 rounded mt-4 justify-content: flex-end;'>
+          Welcome!
+        </button>
       </div>
     </>
   )
