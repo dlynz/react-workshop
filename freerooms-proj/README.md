d23c3f495b18fe2fff83f911a4aaeff84b172369
Dylan Zhang <z5478419@ad.unsw.edu.au>
2025-04-22 17:58:31 +1000

    sync with tailwind setup

---
+++ freerooms-proj/README.md
@@ -0,0 +1,4 @@
+Your task is to use react, html, css (tailwind) to copy this small simple frontend version of the rooms page for Freerooms.
+
+The link to the reference can be found here:
+https://freerooms-copy-oqnw.vercel.app/
---
+++ freerooms-proj/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className=''>
+        {/* START HERE */}
+        <p className='font-bold text-2xl'>
+          Hellooooo!!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
