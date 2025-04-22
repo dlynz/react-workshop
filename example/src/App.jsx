ac2a924b4956fbcd1daf778eb3283e17a08a91f4
Lukairon <kai.r.lu@outlook.com>
2025-04-22 19:17:51 +1000

    added count button

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+function Welcome(props) {
+  return <h1>Hello, {props.name}!</h1>
+}
 
 function App() {
   return (
     <>
-      <div className=''>
+      <div className='flex justify-center'>
         {/* START HERE */}
         <p className='font-bold text-2xl'>
           Hellooooo!!
+          <Welcome name="Kai"/>
         </p>
+        <button className='bg-blue-500 hover:bg-red-700 duration-150'>
+          Click
+        </button>
       </div>
     </>
   )
